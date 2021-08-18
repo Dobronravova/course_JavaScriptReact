88333a12057e2fc8acce2baba563c430a3c8e965
k.dobronravova <k.dobronravova@coretechs.ru>
2021-08-18 16:58:17 +0300

    Lesson 14 - while, for

---
+++ Lesson14/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+//Циклы
+
+let num = 50;
+
+// While
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// Do while
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+
+// For
+
+for (let i = 1; i < 10; i++){
+    if (i === 6){
+        // break;
+        continue;
+    }
+    console.log(i);
+}
