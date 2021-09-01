972f8bacda787818ad8e63f87c88d64bb3ecfdc0
k.dobronravova <k.dobronravova@coretechs.ru>
2021-09-01 11:52:14 +0300

    Lesson 57 - array methods

--- Lesson57/script.js
+++ Lesson57/script.js
@@ -29,13 +29,46 @@ console.log(some.every(item => typeof(item) === 'number'));
 
 //reduce - для сбора массива в единое целое
 
-const arr = [4, 5, 1, 3, 2, 6];
+// const arr = [4, 5, 1, 3, 2, 6];
+//                         // 0 3     4
+//                         // 4       5
+//                         // 9       1
+//                         // 10      3
+// const res = arr.reduce((sum, current) => sum + current, 3); // 3 - начальное значение
+// console.log(res);
 
-                        // 0       4
-                        // 4       5
-                        // 9       1
-                        // 10      3
-const res = arr.reduce((sum, current) => sum + current);
+const arr = ['apple', 'pear', 'plum'];
+
+const res = arr.reduce((sum, current) => `${sum}, ${current}`, 6);
+console.log(res);
+
+
+
+const obj = {
+    ivan: 'persone',
+    ann: 'persone',
+    dog: 'animal',
+    cat: 'animal'
+};
+
+
+//вытащить имена
+
+//entries - трансформация объекта в массив
+//chaning - запись операций в цепочку
+const newArr = Object.entries(obj)
+.filter(item => item[1] === 'persone') //[ [ 'ivan', 'persone' ], [ 'ann', 'persone' ] ]
+.map(item => item[0]);
+
+// [
+//     [ 'ivan', 'persone' ],
+//     [ 'ann', 'persone' ],
+//     [ 'dog', 'animal' ],
+//     [ 'cat', 'animal' ]
+//   ]
+
+console.log(newArr); 
+
 
 
 
