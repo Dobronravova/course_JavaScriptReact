1dd5ecd572a9dbce682ddf042501c38f1820edf8
k.dobronravova <k.dobronravova@coretechs.ru>
2021-09-23 18:06:02 +0300

    Lesson 93 - styled components

---
+++ Lesson93/src/components/app/app.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
+
+// import'./app.css';
+// import style from './App.module.css';
+import styled from 'styled-components';
+const AppBlock = styled.div`
+.app {
+    margin: 0 auto;
+    max-width: 800px;
+  }  
+`;
+
+const StyledAppBlock = styled(AppBlock)`
+    background-color: gray
+`
+
+const App = () => {
+
+    const data = [
+        {
+            label: 'Going to learn React',
+            important: true,
+            id: 'jhg'
+        },
+        {
+            label: 'That is so good',
+            important: false,
+            id: 'joi'
+        },
+        {
+            label: 'I need a break...',
+            important: false,
+            id: 'jvg'
+        }
+    ];
+
+    return (
+        <StyledAppBlock>
+            <AppHeader/>
+            <div className="search-panel d-flex">
+                <SearchPanel/>
+                <PostStatusFilter/>
+            </div>
+            <PostList posts={data}/>
+            <PostAddForm/>
+        </StyledAppBlock>
+    )
+}
+
+export default App;
