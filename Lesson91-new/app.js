cbe16023264179140f3fd6f81e3ac677063f69ce
k.dobronravova <k.dobronravova@coretechs.ru>
2021-09-27 18:14:53 +0300

    Lesson 91 - props view

---
+++ Lesson91-new/app.js
@@ -0,0 +1,36 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+function WhoAmI({name, surname, link}) {
+  return (
+    <div>
+        <h1>My name is {name()}, surname - {surname}</h1>
+        <a href={link}>My profile</a>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="app">
+      <WhoAmI name={() => {return 'John'}} surname='Smith' link='facebook.com'/>
+      <WhoAmI name={() => {return 'Alex'}} surname='Shepard' link='vk.com'/>
+        <AppInfo />
+
+        <div className="search-panel">
+            <SearchPanel/>
+            <AppFilter/>
+        </div>
+        
+        <EmployeesList/>
+        <EmployeesAddForm/>
+    </div>
+  );
+}
+
+export default App;
