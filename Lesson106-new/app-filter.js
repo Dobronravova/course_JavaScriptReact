2e27da8426749d13cc71d8a3209de86815d3bde8
k.dobronravova <k.dobronravova@coretechs.ru>
2021-10-01 12:51:05 +0300

    Lesson 106 - Динамические стили и классы

---
+++ Lesson106-new/app-filter.js
@@ -0,0 +1,44 @@
+import "./app-filter.css";
+
+const AppFilter = (props) => {
+    const buttonsData = [
+        {name: "all", label: 'Все сотрудники', colored: false},
+        {name: "rice", label: 'На повышение', colored: false},
+        {name: "moreThen1000", label: 'З/П больше 1000$', colored: true},
+    ];
+
+    const buttons = buttonsData.map(({name, label, colored}) => {
+        const active = props.filter === name;
+    //Динамический класс
+        const style = colored ? {color: 'red'} : null;
+        const clazz = active ? 'btn-light' : 'btn-outline-light';
+        return (
+            <button type="button"
+                    className={`btn ${clazz}`}
+                    key={name}
+                    onClick={() => props.onFilterSelect(name)}
+                    style={style}>
+                    {label}
+            </button>
+        )
+    })
+    return (
+        <div className="btn-group">
+            {buttons}
+            {/* <button type="button"
+                    className="btn btn-light">
+                    Все сотрудники
+            </button>
+            <button type="button"
+                    className="btn btn-outline-light">
+                    На повышение
+            </button>
+            <button type="button"
+                    className="btn btn-outline-light">
+                    З/П больше 1000$
+            </button> */}
+        </div>
+    )
+}
+
+export default AppFilter;
